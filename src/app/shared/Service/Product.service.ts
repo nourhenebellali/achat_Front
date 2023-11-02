f93cf4ec861c2c743f0bd5851abeb081f2d735d2
nourhenebellali <92648444+nourhenebellali@users.noreply.github.com>
2023-11-01 17:39:15 -0700

    Update Product.service.ts1

--- src/app/shared/Service/Product.service.ts
+++ src/app/shared/Service/Product.service.ts
@@ -6,7 +6,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductService {
-  readonly API_URL = 'http://192.168.140.35:8082/SpringMVC/produit';
+  readonly API_URL = 'http://192.168.100.154:8082/SpringMVC/produit';
 
   constructor(private httpClient: HttpClient) { }
   getAllProducts() {
